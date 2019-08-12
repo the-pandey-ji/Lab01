b71a67cedad541e022507e71ba0e82f6e4fc7dc2
Warrior <sonu.pandey_cs18@gla.ac.in>
2019-08-12 15:31:21 +0530

    add Main.java: add input

---
+++ src/Main/Main.java
@@ -0,0 +1,19 @@
+/*
+ * @author Sonu Pandey
+ * @version 1.0
+ * */
+package Main;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int n = 4;
+        for (int i = 4; i < 11; i++) {
+            System.out.println(n + " X " + i + "=" + (n * i));
+            // x X y = z
+
+        }
+    }
+}
